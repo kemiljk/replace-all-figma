c35f2de74dcec8bc098d45381e5ce1d5b0bd070a
Karl Koch <karl@kejk.tech>
2021-09-04 17:53:48 +0100

    feat: now supports FigJam

--- code.js
+++ code.js
@@ -1,28 +1,41 @@
 figma.showUI(__html__, { width: 300, height: 240 });
 figma.ui.onmessage = (msg) => {
     if (msg.type === "find-and-replace-text") {
-        const selectedNodes = figma.currentPage.selection;
-        const selectedTextNodes = selectedNodes.filter((node) => node.type === "TEXT" && node.characters === msg.findText);
-        const textNodesOnPage = figma.currentPage.findAll((node) => node.type === "TEXT");
-        if (selectedNodes.length >= 1 && selectedTextNodes.length === 0) {
-            figma.notify("Woah there, none of this is text... ‼");
-            return;
-        }
-        if (textNodesOnPage.length === 0) {
-            figma.notify("There's no text here... ∅");
-            return;
-        }
-        const replaceText = async (n) => {
-            // figma.currentPage.selection = textNodesOnPage;
-            for (n of textNodesOnPage) {
-                if (n.type === "TEXT") {
-                    await figma.loadFontAsync(n.fontName);
-                    n.characters = n.characters.replaceAll(msg.findText, msg.replaceText);
-                }
+        if (figma.editorType === "figma") {
+            const selectedNodes = figma.currentPage.selection;
+            const selectedTextNodes = selectedNodes.filter((node) => node.type === "TEXT" && node.characters === msg.findText);
+            const textNodesOnPage = figma.currentPage.findAll((node) => node.type === "TEXT");
+            if (selectedNodes.length >= 1 && selectedTextNodes.length === 0) {
+                figma.notify("Woah there, none of this is text... ‼");
+                return;
             }
-            // figma.notify("Done ✔");
-        };
-        replaceText(textNodesOnPage && figma.notify("Done ✔"));
+            if (textNodesOnPage.length === 0) {
+                figma.notify("There's no text here... ∅");
+                return;
+            }
+            const replaceText = async (n) => {
+                for (n of textNodesOnPage) {
+                    if (n.type === "TEXT") {
+                        await figma.loadFontAsync(n.fontName);
+                        n.characters = n.characters.replaceAll(msg.findText, msg.replaceText);
+                    }
+                }
+            };
+            replaceText(textNodesOnPage && figma.notify("Done ✔"));
+        }
+        else if (figma.editorType === "figjam") {
+            const nodes = figma.currentPage.findAll();
+            nodes.forEach(async (node) => {
+                if (node.type === "SHAPE_WITH_TEXT" || node.type === "STICKY") {
+                    await figma.loadFontAsync(node.text.fontName);
+                    node.text.characters = node.text.characters.replaceAll(msg.findText, msg.replaceText);
+                }
+                if (node.type === "TEXT") {
+                    await figma.loadFontAsync(node.fontName);
+                    node.characters = node.characters.replaceAll(msg.findText, msg.replaceText);
+                }
+            });
+        }
     }
     if (msg.checkboxOn === true) {
         setTimeout(figma.closePlugin, 500);
